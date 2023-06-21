cb0b8957f761d3d7d9f64f9d69ce73b428177508
Asakushen <74193315+Asakushen@users.noreply.github.com>
2023-06-22 00:05:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -86,7 +86,7 @@ function time() {
 //获取天气
 //每日限量 100 次
 //请前往 https://www.tianqiapi.com/index/doc?version=v6 申请（免费）
-fetch('https://yiketianqi.com/api?unescape=1&version=v6&appid=43986679&appsecret=TksqGZT7')
+fetch('https://v0.yiketianqi.com/api?unescape=1&version=v61&appid=23935563&appsecret=E9gYeMJp')
     .then(response => response.json())
     .then(data => {
         //$('#wea_text').html(data.wea + '&nbsp;' + data.tem_night + '℃' + '&nbsp;~&nbsp;' + data.tem_day + '℃')
